adf87653f18fb4b5ad7e96451c8fe9514d20c8ce
Matt Muldowney <matt.muldowney@gmail.com>
2024-03-23 18:05:13 -0700

    update flake

--- config/plugins/completions.nix
+++ config/plugins/completions.nix
@@ -1,50 +1,40 @@
 {
   plugins = {
-    nvim-cmp = {
+    cmp = {
       enable = true;
-      snippet.expand = "luasnip";
-      mapping = {
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-n>" = {
-          action = "cmp.mapping.select_next_item()";
-          modes = [ "i" "s" ];
-        };
-        "<C-p>" = {
-          action = "cmp.mapping.select_prev_item()";
-          modes = [ "i" "s" ];
-        };
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-y>" = "cmp.mapping.confirm({ select = true })";
-        "<C-e>" = "cmp.mapping.close()";
-        "<C-l>" = {
-          action = ''cmp.mapping(function(fallback)
+      settings = {
+        snippet.expand = "luasnip";
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-y>" = "cmp.mapping.confirm({ select = true })";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-l>" = ''cmp.mapping(function(fallback)
               local luasnip = require("luasnip")
               if luasnip.jumpable(1) then
                 luasnip.jump(1)
               else
                 fallback()
               end
-            end)'';
-          modes = [ "i" "s" ];
-        };
-        "<C-h>" = {
-          action = ''cmp.mapping(function(fallback)
+            end, {"i", "s"})'';
+          "<C-h>" = ''cmp.mapping(function(fallback)
               local luasnip = require("luasnip")
               if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
               else
                 fallback()
               end
-            end)'';
-          modes = [ "i" "s" ];
+            end, {"i", "s"})'';
         };
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          { name = "buffer"; }
+        ];
       };
-      sources = [
-        { name = "nvim_lsp"; }
-        { name = "luasnip"; }
-        { name = "buffer"; }
-      ];
     };
 
     luasnip = {
