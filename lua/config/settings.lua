c13f03e29e6e1287e67456f6103a6e3a5d6aceb4
Matt Muldowney <matt.muldowney@gmail.com>
2025-09-15 00:59:07 -0700

    initial config (includes lua and nix lsp config)

---
+++ lua/config/settings.lua
@@ -0,0 +1,18 @@
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
+vim.o.nu = true
+vim.o.relativenumber = true
+
+vim.o.wrap = false
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.termguicolors = true
+
+vim.o.signcolumn = "yes"
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
+		})
+	end,
+}
